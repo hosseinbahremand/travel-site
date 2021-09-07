29dccbd22a78c0a90092ce91ba5cfbc9ab3f260a
Hossein Bahremand <hoseinbahremand@gmail.com>
2021-09-07 16:29:39 +0430

    complete header section

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,10 @@
 import "../styles/styles.css";
+import MobileMenu from "./MobileMenu";
 
 if (module.hot) {
   module.hot.accept(function (err) {
     console.log("An error occurred while accepting new version");
   });
 }
+
+const mobileMenu = new MobileMenu();
