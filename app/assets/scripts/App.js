9d4b9bdfcc56e2571928d9cbee506d0167654238
Hossein Bahremand <hoseinbahremand@gmail.com>
2021-09-12 12:03:18 +0430

    adding modal and lazyload

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,15 +1,35 @@
 import "../styles/styles.css";
+import "lazysizes";
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader";
+
+new MobileMenu();
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
+new RevealOnScroll(document.querySelectorAll(".testimonials"), 60);
+new StickyHeader();
+let modal;
+
+document.querySelectorAll(".open-modal").forEach((el) => {
+  el.addEventListener("click", (e) => {
+    if (modal === undefined) {
+      e.preventDefault();
+      import("./modules/Modal")
+        .then((res) => {
+          modal = new res.default();
+          modal.openModal();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      modal.openModal();
+    }
+  });
+});
 
 if (module.hot) {
   module.hot.accept(function (err) {
     console.log("An error occurred while accepting new version");
   });
 }
-
-new MobileMenu();
-new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
-new RevealOnScroll(document.querySelectorAll(".testimonials"), 60);
-new StickyHeader();
