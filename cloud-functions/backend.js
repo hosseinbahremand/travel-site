f28dbe9e39dcf03e4e17f5ba399b8923b2c6ed46
Hossein Bahremand <hoseinbahremand@gmail.com>
2021-09-15 15:43:43 +0430

    adding secret area

--- cloud-functions/backend.js
+++ cloud-functions/backend.js
@@ -1,7 +1,25 @@
 exports.handler = function (event, context, callback) {
-  console.log("betg");
-  callback(null, {
-    statusCode: 200,
-    body: JSON.parse(event),
-  });
+  const secretContent = `
+  <h3>Welcome To The Secret Area</h3>
+  <p>Here we can tell you that the sky is <strong>blue</strong>, and two plus two equals four.</p>
+  `;
+
+  let body;
+
+  if (event.body) {
+    body = JSON.parse(event.body);
+  } else {
+    body = {};
+  }
+
+  if (body.password === "shabaliscoo") {
+    callback(null, {
+      statusCode: 200,
+      body: secretContent,
+    });
+  } else {
+    callback(null, {
+      statusCode: 401,
+    });
+  }
 };
