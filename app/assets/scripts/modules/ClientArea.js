d29b9094d18a4dc00b2732a3837558f2895247d9
Hossein Bahremand <hoseinbahremand@gmail.com>
2021-09-15 15:49:21 +0430

    adding secret area

--- app/assets/scripts/modules/ClientArea.js
+++ app/assets/scripts/modules/ClientArea.js
@@ -1,34 +1,41 @@
-import Axios from 'axios'
+import Axios from "axios";
 
 class ClientArea {
   constructor() {
-    this.injectHTML()
-    this.form = document.querySelector(".client-area__form")
-    this.field = document.querySelector(".client-area__input")
-    this.contentArea = document.querySelector(".client-area__content-area")
-    this.events()
+    this.injectHTML();
+    this.form = document.querySelector(".client-area__form");
+    this.field = document.querySelector(".client-area__input");
+    this.contentArea = document.querySelector(".client-area__content-area");
+    this.events();
   }
 
   events() {
-    this.form.addEventListener("submit", e => {
-      e.preventDefault()
-      this.sendRequest()
-    })
+    this.form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      this.sendRequest();
+    });
   }
 
   sendRequest() {
-    Axios.post('https://pensive-rosalind-383c21.netlify.com/.netlify/functions/secret-area', {password: this.field.value}).then(response => {
-      this.form.remove()
-      this.contentArea.innerHTML = response.data
-    }).catch(() => {
-      this.contentArea.innerHTML = `<p class="client-area__error">That secret phrase is not correct. Try again.</p>`
-      this.field.value = ''
-      this.field.focus()
-    })
+    Axios.post(
+      "https://wizardly-ritchie-022f6b.netlify.app/.netlify/functions/backend",
+      { password: this.field.value }
+    )
+      .then((response) => {
+        this.form.remove();
+        this.contentArea.innerHTML = response.data;
+      })
+      .catch(() => {
+        this.contentArea.innerHTML = `<p class="client-area__error">That secret phrase is not correct. Try again.</p>`;
+        this.field.value = "";
+        this.field.focus();
+      });
   }
 
   injectHTML() {
-    document.body.insertAdjacentHTML('beforeend', `
+    document.body.insertAdjacentHTML(
+      "beforeend",
+      `
     <div class="client-area">
       <div class="wrapper wrapper--medium">
         <h2 class="section-title section-title--blue">Secret Client Area</h2>
@@ -39,8 +46,9 @@ class ClientArea {
         <div class="client-area__content-area"></div>
       </div>
     </div>
-    `)
+    `
+    );
   }
 }
 
-export default ClientArea
+export default ClientArea;
